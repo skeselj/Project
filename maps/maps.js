fe9b6aca0fe9c19e576f1a1e2e08ad277c446e7f
Stefan Keselj <skeselj@princeton.edu>
2016-03-30 21:56:42 -0400

    formatted map

--- maps/maps.js
+++ maps/maps.js
@@ -1,11 +1,154 @@
 // Meteor
 PointsList = new Mongo.Collection('points')
+
 if (Meteor.isClient) {
+
+  // EXPERIMENTAL
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  }); 
+  Template.body.helpers({
+    exampleMapOptions: function() {
+      if (GoogleMaps.loaded()) {
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(40.7148544,-74.0166855),
+          zoom: 14,
+
+          // How you would like to style the map. 
+          // This is where you would paste any style found on Snazzy Maps.
+          styles: [{
+              "featureType": "water",
+              "elementType": "geometry",
+              "stylers": [{
+                 "color": "#000000"
+             }, {
+                 "lightness": 17
+             }]
+          }, {
+              "featureType": "landscape",
+             "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 20
+              }]
+          }, {
+              "featureType": "road.highway",
+              "elementType": "geometry.fill",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 17
+             }]
+         }, {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 29
+              }, {
+                  "weight": 0.2
+             }]
+          }, {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                 "lightness": 18
+              }]
+         }, {
+             "featureType": "road.local",
+             "elementType": "geometry",
+             "stylers": [{
+                 "color": "#000000"
+             }, {
+                 "lightness": 16
+             }]
+         }, {
+             "featureType": "poi",
+             "elementType": "geometry",
+             "stylers": [{
+                 "color": "#000000"
+             }, {
+                 "lightness": 21
+             }]
+         }, {
+             "elementType": "labels.text.stroke",
+             "stylers": [{
+                 "visibility": "on"
+             }, {
+                 "color": "#000000"
+             }, {
+                  "lightness": 16
+             }]
+         }, {
+             "elementType": "labels.text.fill",
+             "stylers": [{
+                 "saturation": 36
+             }, {
+                 "color": "#000000"
+             }, {
+                 "lightness": 40
+             }]
+         }, {
+             "elementType": "labels.icon",
+             "stylers": [{
+                 "visibility": "off"
+             }]
+         }, {
+             "featureType": "transit",
+             "elementType": "geometry",
+             "stylers": [{
+                 "color": "#000000"
+             }, {
+                 "lightness": 19
+             }]
+         }, {
+              "featureType": "administrative",
+             "elementType": "geometry.fill",
+             "stylers": [{
+                 "color": "#000000"
+              }, {
+                 "lightness": 20
+             }]
+         }, {
+             "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [{
+                 "color": "#000000"
+             }, {
+                  "lightness": 17
+              }, {
+                 "weight": 1.2
+              }]
+          }]
+          };
+        }
+     }
+    });
+
+
+  Template.body.onCreated(function() {
+    // We can use the `ready` callback to interact with the map API once the map is ready.
+    GoogleMaps.ready('exampleMap', function(map) {
+      // Add a marker to the map once it's ready
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+    });
+  });
+  
+
+  // TESTED
   Template.board.helpers({
     'point': function() {
       return PointsList.find({}, {sort: {long: 1}});
-    }
-  });  
+    },
+  });
 
   Template.board.events({
     'click .point': function() {
@@ -16,7 +159,7 @@ if (Meteor.isClient) {
       var selectedPoint = Session.get('selectedPoint');
       PointsList.remove(selectedPoint);
     }
-  })
+  });
 
   Template.addPointForm.events({
     'submit form': function(event) {
@@ -29,14 +172,5 @@ if (Meteor.isClient) {
       });
     }
   });
-}
-
-// Google Maps
-function initMap() {
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-        center: {lat: 40.6842763, lng: -74.0127941},
-        zoom: 12
-    });
 }
 
