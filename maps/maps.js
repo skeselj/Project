6f42e8683e641e24e40691d3cfa54aba5f78913c
Stefan Keselj <skeselj@princeton.edu>
2016-03-31 23:11:25 -0400

    layout

--- maps/maps.js
+++ maps/maps.js
@@ -49,137 +49,23 @@ if (Meteor.isClient) {
           zoom: 12,
           disableDefaultUI: true,
           zoomControl: true,
+          zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER
+            },
         };
       }
     }
   });
 }
 
-/*
- 
-        // Map initialization options
-        return {
-          center: new google.maps.LatLng(40.7148544,-74.0166855),
-          zoom: 14,
-          disableDefaultUI: true,
-          zoomControl: true,
-
-          // Styles found on Snazzy Maps.
-          styles: [{
-              "featureType": "water",
-              "elementType": "geometry",
-              "stylers": [{
-                 "color": "#000000"
-             }, {
-                 "lightness": 17
-             }]
-          }, {
-              "featureType": "landscape",
-             "elementType": "geometry",
-              "stylers": [{
-                  "color": "#000000"
-              }, {
-                  "lightness": 20
-              }]
-          }, {
-              "featureType": "road.highway",
-              "elementType": "geometry.fill",
-              "stylers": [{
-                  "color": "#000000"
-              }, {
-                  "lightness": 17
-             }]
-         }, {
-              "featureType": "road.highway",
-              "elementType": "geometry.stroke",
-              "stylers": [{
-                  "color": "#000000"
-              }, {
-                  "lightness": 29
-              }, {
-                  "weight": 0.2
-             }]
-          }, {
-              "featureType": "road.arterial",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#000000"
-              }, {
-                 "lightness": 18
-              }]
-         }, {
-             "featureType": "road.local",
-             "elementType": "geometry",
-             "stylers": [{
-                 "color": "#000000"
-             }, {
-                 "lightness": 16
-             }]
-         }, {
-             "featureType": "poi",
-             "elementType": "geometry",
-             "stylers": [{
-                 "color": "#000000"
-             }, {
-                 "lightness": 21
-             }]
-         }, {
-             "elementType": "labels.text.stroke",
-             "stylers": [{
-                 "visibility": "on"
-             }, {
-                 "color": "#000000"
-             }, {
-                  "lightness": 16
-             }]
-         }, {
-             "elementType": "labels.text.fill",
-             "stylers": [{
-                 "saturation": 36
-             }, {
-                 "color": "#000000"
-             }, {
-                 "lightness": 40
-             }]
-         }, {
-             "elementType": "labels.icon",
-             "stylers": [{
-                 "visibility": "off"
-             }]
-         }, {
-             "featureType": "transit",
-             "elementType": "geometry",
-             "stylers": [{
-                 "color": "#000000"
-             }, {
-                 "lightness": 19
-             }]
-         }, {
-              "featureType": "administrative",
-             "elementType": "geometry.fill",
-             "stylers": [{
-                 "color": "#000000"
-              }, {
-                 "lightness": 20
-             }]
-         }, {
-             "featureType": "administrative",
-              "elementType": "geometry.stroke",
-              "stylers": [{
-                 "color": "#000000"
-             }, {
-                  "lightness": 17
-              }, {
-                 "weight": 1.2
-              }]
-            }]
-          };
-        }
+if (Meteor.isServer) {
+  // Debugging helper as I figure out how to insert data points
+  Meteor.startup(function() {
+    return Meteor.methods({
+      removeAllMarkers: function() {
+        return Markers.remove({});
       }
     });
-*/
-  
-/*
-// 857f72c1b5434c6b8739efd7be52b96b63a3887d
-*/
+  });
+}
 
