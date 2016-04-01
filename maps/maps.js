a97b63568a39c3530126b66534c95d31be734fee
Stefan Keselj <skeselj@princeton.edu>
2016-04-01 01:19:57 -0400

    added kavin's shitty code

--- maps/maps.js
+++ maps/maps.js
@@ -52,9 +52,9 @@ if (Meteor.isClient) {
           zoomControlOptions: {
             position: google.maps.ControlPosition.LEFT_CENTER
             },
-        };
+          };
+        }
       }
-    }
   });
 }
 
